8e19c89ac3d57e4cd889ac7344fb2b291c1e8b83
Minebot <serpinin@mail.ru>
2021-04-05 11:18:53 +0300

    [feat] mvvm and frontend

---
+++ CityExplorer/ApplicationViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CityExplorer.Annotations;
+
+namespace CityExplorer
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> FederationSubjects { get; set; }
+        public ObservableCollection<string> CommunityTypes { get; set; }
+        public ObservableCollection<Community> Communities { get; set; }
+
+        private Community selectedCommunity;
+        
+        public Community SelectedCommunity
+        {
+            get => selectedCommunity;
+            set
+            {
+                selectedCommunity = value;
+                OnPropertyChanged(nameof(SelectedCommunity));
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Communities = new ObservableCollection<Community>()
+            {
+                new () { CommunityType = "поселок", FederationSubject = "Московская область", Population = 1000, Title = "Какой-то поселок", FoundationYear = 1920},
+                new () { CommunityType = "город", FederationSubject = "Волгоградская область", Population = 2000, Title = "Какой-то город", FoundationYear = 1990},
+                new () { CommunityType = "деревня", FederationSubject = "Ростовская область", Population = 3000, Title = "Какая-то деревня", FoundationYear = 1950}
+            };
+
+            FederationSubjects = new ObservableCollection<string>() { "Волгоградская область", "Московская область", "Ростовская область" };
+            CommunityTypes = new ObservableCollection<string>() { "город", "поселок", "деревня" };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ CityExplorer/Community.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CityExplorer.Annotations;
+
+namespace CityExplorer
+{
+    public class Community : INotifyPropertyChanged
+    {
+        private string federationSubject = "";
+        private string communityType = "";
+        private string title = "";
+        private int population;
+        private int foundationYear;
+
+        public string FederationSubject
+        {
+            get => federationSubject;
+            set
+            {
+                federationSubject = value;
+                OnPropertyChanged(nameof(FederationSubject));
+            }
+        }
+        
+        public string CommunityType
+        {
+            get => communityType;
+            set
+            {
+                communityType = value;
+                OnPropertyChanged(nameof(CommunityType));
+            }
+        }
+        
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        
+        public int Population
+        {
+            get => population;
+            set
+            {
+                population = value;
+                OnPropertyChanged(nameof(Population));
+            }
+        }
+        
+        public int FoundationYear
+        {
+            get => foundationYear;
+            set
+            {
+                foundationYear = value;
+                OnPropertyChanged(nameof(FoundationYear));
+            }
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
